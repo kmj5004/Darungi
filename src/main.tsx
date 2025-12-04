1938fa46ceeaed54420f0ad58f59d7558a3b7586
kmj5004 <kmjmj8769@gmail.com>
2025-12-04 10:21:20 +0900

    Add PWA support with Service Worker and icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { registerSW } from "virtual:pwa-register";
 import App from './App.tsx'
 
 import "./index.css";
+
+registerSW({
+  onNeedRefresh() {
+    console.log("새 버전이 준비되었습니다.");
+  },
+  onOfflineReady() {
+    console.log("오프라인 사용 준비 완료");
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
